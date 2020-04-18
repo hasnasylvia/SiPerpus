1076cf895bfb7505ff81f05c2a72cd7861a6a213
hasnasylvia <53207279+hasnasylvia@users.noreply.github.com>
2020-04-18 12:19:36 +0700

    Add files via upload

---
+++ asset/footer.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <script src="http://localhost/siperpus/asset/jquery.js"></script>
+        <script src="http://localhost/siperpus/asset/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
